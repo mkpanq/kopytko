3f57d6ab86f134a6529147104f54ff83da2d94cc
Marek Pankowski <17934750+mkpanq@users.noreply.github.com>
2024-05-27 21:06:54 +0200

    Add some turbo.json config for DB to make migration terminal interfactive, add zod schemas from table

--- src/backend/db/schemas/user.ts
+++ src/backend/db/schemas/user.ts
@@ -2,7 +2,7 @@ import { serial, text, timestamp, pgTable } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
   id: serial("id").primaryKey(),
-  name: text("name").notNull(),
+  username: text("username").notNull(),
   email: text("email").notNull().unique(),
   password: text("password").notNull(),
   createdAt: timestamp("created_at").notNull(),
---
+++ src/backend/shared/schemas/user.ts
@@ -0,0 +1,41 @@
+import { createInsertSchema } from "drizzle-zod";
+import z from "zod";
+import { user } from "../../db/schemas/user";
+
+export const ZCreateUserSchema = createInsertSchema(user, {
+  username: (schema) =>
+    schema.username.min(1, {
+      message: "Username must be at least 1 character long",
+    }),
+  email: (schema) =>
+    schema.email.email({
+      message: "Email must be a valid email address",
+    }),
+  password: (schema) =>
+    schema.password.min(8, {
+      message: "Password must be at least 8 characters long",
+    }),
+});
+
+// For validation on frontend forms
+export const ZSignupUserSchemaFormValidation = ZCreateUserSchema.extend({
+  password_confirmation: z.string(),
+})
+  .pick({
+    username: true,
+    email: true,
+    password: true,
+    password_confirmation: true,
+  })
+  .refine((data) => data.password === data.password_confirmation, {
+    message: "Passwords do not match",
+    path: ["password_confirmation"],
+  });
+
+export const ZLoginUserSchemaFormValidation = ZCreateUserSchema.pick({
+  email: true,
+  password: true,
+});
+
+export type TLoginUserSchema = z.infer<typeof ZLoginUserSchemaFormValidation>;
+export type TSignupUserSchema = z.infer<typeof ZSignupUserSchemaFormValidation>;
